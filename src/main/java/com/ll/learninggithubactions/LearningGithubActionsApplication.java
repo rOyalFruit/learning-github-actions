716ecfd828be291651317259ab2549fecf47467a
rOyalFruit <ekdeon@gmail.com>
2025-03-19 14:34:32 +0900

    프로젝트 생성

---
+++ src/main/java/com/ll/learninggithubactions/LearningGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.ll.learninggithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningGithubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/learninggithubactions/LearningGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.learninggithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
