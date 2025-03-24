83f8e5c63e3588e828d1e0790cb9288c79e0a4d1
rOyalFruit <ekdeon@gmail.com>
2025-03-25 00:34:26 +0900

    feat: checkstyle 추가

--- src/main/java/com/ll/learninggithubactions/LearningGithubActionsApplication.java
+++ src/main/java/com/ll/learninggithubactions/LearningGithubActionsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearningGithubActionsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LearningGithubActionsApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(LearningGithubActionsApplication.class, args);
+	}
 }
