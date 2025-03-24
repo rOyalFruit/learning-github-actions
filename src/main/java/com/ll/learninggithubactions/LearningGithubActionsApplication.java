c161e2567b31a68028e0067b82d8050e7d14cddd
rOyalFruit <ekdeon@gmail.com>
2025-03-25 01:04:58 +0900

    stylecheck 테스트

--- src/main/java/com/ll/learninggithubactions/LearningGithubActionsApplication.java
+++ src/main/java/com/ll/learninggithubactions/LearningGithubActionsApplication.java
@@ -8,5 +8,11 @@ public class LearningGithubActionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningGithubActionsApplication.class, args);
+		int a = 0;
+
+		for (int i = a; i < 10; i++) {
+			System.out.println(i);
+		}
+
 	}
 }
