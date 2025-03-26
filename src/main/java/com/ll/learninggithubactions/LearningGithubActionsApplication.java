29d5e551e41310fabd912fcc05d9fca53d0092d2
rOyalFruit <ekdeon@gmail.com>
2025-03-26 11:08:51 +0900

    yml 수정

--- src/main/java/com/ll/learninggithubactions/LearningGithubActionsApplication.java
+++ src/main/java/com/ll/learninggithubactions/LearningGithubActionsApplication.java
@@ -2,11 +2,19 @@ package com.ll.learninggithubactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LearningGithubActionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningGithubActionsApplication.class, args); // 빌드 실패 테스트를 위해 세미콜론 누락
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello, World!";
 	}
 }
